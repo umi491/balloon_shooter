4064401b0a768d23a99b3cb1654831e16a82caa2
Michelle <ljleem5475@gmail.com>
2022-02-01 13:36:51 +1100

    enemy ver 1

--- app.py
+++ app.py
@@ -19,8 +19,10 @@
 # Updating the number of misses
 # Handling end of game
 
-import pygame, random
+import pygame
 import os
+from random import seed, randint
+from time import time
 
 
 # defines
@@ -29,11 +31,24 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 FPS = 60
 PLAYER_VEL = 5
-ENEMY_VEL = 2
+
+RANDOM_CHANCE = 5
+FRAMES = 25
 
 PLAYER_WIDTH, PLAYER_HEIGHT = 100, 100
 ENEMY_WIDTH, ENEMY_HEIGHT = 80, 80
 
+
+all_sprites = pygame.sprite.Group()
+balloongroup = pygame.sprite.Group()
+seed(time())
+
+
+def random_start_direction():
+    if randint(0, 1):
+        return 1
+    else:
+        return -1
 
 # set up assets
 BACKGROUND = pygame.image.load(os.path.join('assets', 'background.jpg'))
@@ -53,10 +68,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pokeball Shooter")
 
 
-def draw_screen(player, enemy):
+def draw_screen(player):
     screen.blit(BACKGROUND, (0, 0))
     screen.blit(PLAYER_IMAGE, (player.x, player.y))
-    screen.blit(ENEMY_IMAGE, (enemy.x, enemy.y))
     pygame.display.update()
 
 
@@ -67,17 +81,60 @@ def handle_player_movement(press_keys, player):
         player.y += PLAYER_VEL
 
 
-# testing enemy movement
-def handle_enemy_movement(enemy):
-    
-    if enemy.y - ENEMY_VEL > 0:
-        enemy.y -= ENEMY_VEL
+class Balloon(pygame.sprite.Sprite):
+    def __init__(self, vector):
+        pygame.sprite.Sprite.__init__(self)
+        image = ENEMY_IMAGE
+        self.image = image
+        self.rect = image.get_rect()
+        self.area = screen.get_rect()
+        self.vector = vector
+        self.move_count = 0
+        self.rect.midright = self.area.midright
+
+    def update(self):
+        newpos = self.calcnewpos(self.rect, self.vector)
+        self.rect = newpos
+        (angle, z) = self.vector
+
+        if not self.area.contains(newpos):
+            tl = not self.area.collidepoint(newpos.topleft)
+            tr = not self.area.collidepoint(newpos.topright)
+            bl = not self.area.collidepoint(newpos.bottomleft)
+            br = not self.area.collidepoint(newpos.bottomright)
+            if tr and tl or (br and bl):
+                angle = -angle
+
+        self.vector = (angle,z)
+        self.move_count += 1
+
+    def calcnewpos(self, rect, vector):
+        (angle, z) = vector
+        if self.move_count >= FRAMES:
+            if self.random_direction():
+                self.vector = (-angle, z)
+            self.move_count = 0
+        (dx, dy) = (z * 0, z * angle)
+        return rect.move(dx, dy)
+
+    def random_direction(self):
+        if randint(0, RANDOM_CHANCE) == 0:
+            return True
 
 
 # main game loop
 def main():
+
+    # Set the game speed and random generator
+    speed = 2
+    seed(time())
+
+    # Initialise player and enemy
     player = pygame.Rect(20, 400, PLAYER_WIDTH, PLAYER_HEIGHT)
-    enemy = pygame.Rect(620, 425, ENEMY_WIDTH, ENEMY_HEIGHT)
+    balloon = Balloon((random_start_direction(), speed))
+
+    all_sprites.add(balloon)
+    balloongroup.add(balloon)
     
     run = True
     while run:
@@ -86,11 +143,15 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+        screen.blit(BACKGROUND, balloon.rect, balloon.rect)
+        all_sprites.update()
+
         press_keys = pygame.key.get_pressed()
         handle_player_movement(press_keys, player)
-        handle_enemy_movement(enemy)
         
-        draw_screen(player, enemy)
+        draw_screen(player)
+        all_sprites.draw(screen)
+        pygame.display.flip()
 
     pygame.quit()
 
