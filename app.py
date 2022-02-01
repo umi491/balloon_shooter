d804b6917a13e8d8301510815fe1ecb9690b63fb
Michelle <ljleem5475@gmail.com>
2022-02-01 19:02:21 +1100

    test

--- app.py
+++ app.py
@@ -21,6 +21,9 @@
 
 import pygame, random
 import os
+from time import time
+from random import randint, uniform
+vec = pg.math.Vector2
 
 
 # defines
@@ -29,10 +32,10 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 FPS = 60
 PLAYER_VEL = 5
-ENEMY_VEL = 2
+BALLOON_VEL = 2
 
 PLAYER_WIDTH, PLAYER_HEIGHT = 100, 100
-ENEMY_WIDTH, ENEMY_HEIGHT = 80, 80
+BALLOON_WIDTH, BALLOON_HEIGHT = 80, 80
 
 
 # set up assets
@@ -42,8 +45,11 @@ BACKGROUND = pygame.transform.scale(BACKGROUND, (827, 570))
 PLAYER_IMAGE = pygame.image.load(os.path.join('assets', 'pea_shooter.png'))
 PLAYER_IMAGE = pygame.transform.scale(PLAYER_IMAGE, (PLAYER_WIDTH, PLAYER_HEIGHT))
 
-ENEMY_IMAGE = pygame.image.load(os.path.join('assets', 'pokeball.png'))
-ENEMY_IMAGE = pygame.transform.scale(ENEMY_IMAGE, (ENEMY_WIDTH, ENEMY_HEIGHT))
+BALLOON_IMAGE = pygame.image.load(os.path.join('assets', 'pokeball.png'))
+BALLOON_IMAGE = pygame.transform.scale(BALLOON_IMAGE, (BALLOON_WIDTH, BALLOON_HEIGHT))
+
+# balloon properties
+MAX_SPEED = 5
 
 
 # initialise screen
@@ -53,10 +59,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pokeball Shooter")
 
 
-def draw_screen(player, enemy):
+def draw_screen(player):
     screen.blit(BACKGROUND, (0, 0))
     screen.blit(PLAYER_IMAGE, (player.x, player.y))
-    screen.blit(ENEMY_IMAGE, (enemy.x, enemy.y))
     pygame.display.update()
 
 
@@ -68,16 +73,39 @@ def handle_player_movement(press_keys, player):
 
 
 # testing enemy movement
-def handle_enemy_movement(enemy):
-    
-    if enemy.y - ENEMY_VEL > 0:
-        enemy.y -= ENEMY_VEL
+class Balloon(pygame.sprite.Sprite):
+    """A ball that will move across the screen
+    Returns: ball object
+    Functions: update, calcnewpos
+    Attributes: area, vector"""
+
+    def __init__(self, vector):
+        pygame.sprite.Sprite.__init__(self)
+        image = BALLOON_IMAGE
+        self.image = image
+        self.rect = image.get_rect()
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.vector = vector
+        self.pos = vec(randint(0, WIDTH), randint(0, HEIGHT))
+        self.vel = vec(MAX_SPEED, 0).rotate(uniform(0, 360))
+        self.acc = vec(0, 0)
+        self.rect.center = self.pos
+
+    def update(self):
+        newpos = self.calcnewpos(self.rect,self.vector)
+        self.rect = newpos
+
+    def calcnewpos(self,rect,vector):
+        (angle,z) = vector
+        (dx,dy) = (z*math.cos(angle),z*math.sin(angle))
+        return rect.move(dx,dy)
 
 
 # main game loop
 def main():
     player = pygame.Rect(20, 400, PLAYER_WIDTH, PLAYER_HEIGHT)
-    enemy = pygame.Rect(620, 425, ENEMY_WIDTH, ENEMY_HEIGHT)
+    balloon = balloon()
     
     run = True
     while run:
@@ -88,9 +116,7 @@ def main():
 
         press_keys = pygame.key.get_pressed()
         handle_player_movement(press_keys, player)
-        handle_enemy_movement(enemy)
-        
-        draw_screen(player, enemy)
+        draw_screen(player)
 
     pygame.quit()
 
